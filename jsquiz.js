15c5db78e90ebe5a26fb48636cb66b3162601331
clap <cilapitan1@gmail.com>
2023-04-03 20:21:16 -0700

    quiz questions

---
+++ jsquiz.js
@@ -0,0 +1,16 @@
+var quizQuestions = [   {
+    question: "Where is the correct place to insert a JavaScript?",
+    choices: ["The <body> section", "The <head> section", "Both the <head> and the <bdoy> section are correct"],
+    correctAnswer: "Both the <head> section and the <body> section are correct"
+},
+{
+    question: "The external JavaScript file must contain the <script> tag",
+    choices: ["True", "False"],
+    correctAnswer: "False"
+},
+{
+    question: "How do you create a function in JavaScript?",
+    choices: ["function = myFucnction()", "function:myFunction()", "function myFunction()"],
+    correctAnswer: "function myFunction()"
+},
+]
